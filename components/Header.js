c321d54ea445e11af286c6aea306e3f9056d358d
David Cavar Lacic <davidcavarlacic@gmail.com>
2022-02-01 17:54:49 +0100

    Navigation bar

--- components/Header.js
+++ components/Header.js
@@ -5,17 +5,7 @@ export default function Header({ title }) {
   return (
     <div class="header">
       <ResponsiveAppBar></ResponsiveAppBar>
-      
-     {/* <nav>
-        <ul>
-          <li><Link href="/usluge">Usluge</Link></li>
-          <li><Link href="/onama">O nama</Link></li>
-          <li><Link href="/blog">Arriva Blog</Link></li>
-          <li><Link href="/repozitorij">Repozitorij</Link></li>
-          <li><Link href="/kontakt">Kontakt</Link></li>
-        </ul>
-        
-     </nav>*/}
     </div>
+    
   )
 }
